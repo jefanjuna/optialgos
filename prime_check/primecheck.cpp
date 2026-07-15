1b5c9158bfb4589a003abcced314284cf4b669fa
Josh <devjoshcodes@gmail.com>
2026-07-15 18:54:48 +0800

    added prime checker in c++

---
+++ prime_check/primecheck.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n;
+cin >> n;
+if (n < 2) {
+cout << "Not Prime";
+return 0;
+}
+if (n == 2) {
+cout << "Prime";
+return 0;
+}
+if (n % 2 == 0) {
+cout << "Not Prime";
+return 0;
+}
+for (int i = 2; i*i <= n; i++) {
+if (n % i == 0) {
+cout << "Not Prime";
+return 0;
+}
+}
+cout << "Prime";
+return 0;
+}
