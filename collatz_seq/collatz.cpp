4e70dfdff9205d2f77976bb81c336b47278e164b
Josh <devjoshcodes@gmail.com>
2026-07-15 18:47:08 +0800

    Collatz cpp added
    This C++ program generates the Collatz sequence for a given number

---
+++ collatz_seq/collatz.cpp
@@ -0,0 +1,16 @@
+// This C++ version won't return number of steps but generates the sequence
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	cout << n << " ";
+	while (n != 1) {
+		if (n % 2 == 0) {
+			n /= 2;
+		} else {
+			n = 3*n + 1;
+		}
+		cout << n << " ";
+	}
+}
